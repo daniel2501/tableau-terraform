2f77522f5516aa7da2f23b7dbc08b5346b833b97
Daniel Benedict <daniel2501@gmail.com>
2022-02-24 09:45:17 -0500

    Added AWS secret key and ID to engage.sh

--- engage.sh
+++ engage.sh
@@ -10,12 +10,15 @@ do
         ssh-keygen -b 4096 -t rsa -f $OPTARG -q -N "" )
 done
 
-# Set env vars
+# Set env vars for secrets
 export TF_VAR_os_user_password=$(pass tfts/os_password)
 export TF_VAR_ts_admin_password=$(pass tfts/ts_admin_password)
+export AWS_ACCESS_KEY_ID=$(pass aws/access-key-id)
+export AWS_SECRET_ACCESS_KEY=$(pass aws/secret-access-key)
+export AWS_DEFAULT_REGION=us-east-1
 
-echo -e "Running:\nterraform $1"
-terraform $1
+echo -e "Running:\nterraform ${@}"
+terraform "$@"
 
 unset TF_VAR_os_user_password
 unset TF_VAR_ts_admin_password
